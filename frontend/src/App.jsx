8166f178aec560dbd911fd5c6eb0b4efd6a0b162
Gyubong <jopemachine@naver.com>
2022-11-05 15:28:02 +0900

    feat: Implement frontend App skeleton

---
+++ frontend/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+import ChattingRoomPage from "./pages/chattingRoom";
+import SignInPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
+
+const INITIAL_PAGE = "SignIn";
+
+const App = () => {
+  const [page, setPage] = useState(INITIAL_PAGE);
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    const sessionId = window.localStorage.getItem('session-id');
+    if (sessionId) {
+      setPage('ChattingRoom');
+    }
+  }, []);
+
+  let main;
+  switch (page) {
+    case "ChattingRoom":
+      main = <ChattingRoomPage userId={userId} />;
+      break;
+    case "SignIn":
+      main = <SignInPage setPage={setPage} />;
+      break;
+    case "SignUp":
+      main = <SignUpPage setPage={setPage} />;
+      break;
+    default:
+      console.error('Error, page is not valid value, page: ', page);
+      break;
+  }
+
+  return (
+    <OuterContainer>
+      {main}
+    </OuterContainer>
+  );
+};
+
+const OuterContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+`;
+
+export default App;
